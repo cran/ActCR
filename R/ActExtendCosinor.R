2093db3d6328526d61d1a380de1ce39502147bec
Junrui Di <dijunrui@gmail.com>
2022-05-11 12:40:02 +0000

    version 0.3.0

--- R/ActExtendCosinor.R
+++ R/ActExtendCosinor.R
@@ -6,11 +6,12 @@
 #' @param window The calculation needs the window size of the data. E.g window = 1 means each epoch is in one-minute window.
 #' @param lower A numeric vector of lower bounds on each of the five parameters (in the order of minimum, amplitude, alpha, beta, acrophase) for the NLS. If not given, the default lower bound for each parameter is set to \code{-Inf}.
 #' @param upper A numeric vector of upper bounds on each of the five parameters (in the order of minimum, amplitude, alpha, beta, acrophase) for the NLS. If not given, the default lower bound for each parameter is set to \code{Inf}
+#' @param export_ts A Boolean to indicate whether time series should be exported
 #'
 #' @importFrom cosinor cosinor.lm
 #' @importFrom cosinor2 correct.acrophase
 #' @importFrom minpack.lm nls.lm nls.lm.control
-#' @importFrom stats coef residuals
+#' @importFrom stats coef residuals fitted
 #'
 #' @return A list with elements
 #' \item{minimum}{Minimum value of the of the function.}
@@ -23,21 +24,22 @@
 #' \item{DownMesor}{Time of day of switch from high to low activity. Represents the timing of the rest-activity rhythm. Lower (earlier) values indicate decline in activity earlier in the day, suggesting a more advanced circadian phase.}
 #' \item{MESOR}{A measure analogous to the MESOR of the cosine model (or half the deflection of the curve) can be obtained from mes=min+amp/2. However, it goes through the middle of the peak, and is therefore not equal to the MESOR of the cosine model, which is the mean of the data.}
 #' \item{ndays}{Number of days modeled.}
+#' \item{cosinor_ts}{Exported data frame with time, time over days, original time series, fitted time series using cosinor model from step 1, and fitted extended cosinor model from step 2}
 #'
 #'
 #' @references Marler MR, Gehrman P, Martin JL, Ancoli-Israel S. The sigmoidally transformed cosine curve: a mathematical model for circadian rhythms with symmetric non-sinusoidal shapes. Stat Med.
 #' @export
 #' @examples
-#' count1 = c(t(example_activity_data$count[1,-c(1,2)]))
-#' cos_coeff = ActExtendCosinor(x = count1, window = 1)
+#' count1 = c(t(example_activity_data$count[c(1:2),-c(1,2)]))
+#' cos_coeff = ActExtendCosinor(x = count1, window = 1,export_ts = TRUE)
 
 
 ActExtendCosinor = function(
   x,
   window = 1,
   lower = c(0, 0, -1, 0, -3), ## min, amp, alpha, beta, phi
-  upper = c(Inf, Inf, 1, Inf, 27)
-
+  upper = c(Inf, Inf, 1, Inf, 27),
+  export_ts = FALSE
 ){
   if(1440 %% window != 0){
     stop("Only use window size that is an integer factor of 1440")
@@ -71,11 +73,31 @@ ActExtendCosinor = function(
   e_phi0 = acrotime
   e_par0 = c(e_min0, e_amp0, 0, 2, e_phi0) ## min, amp, alpha, beta, phi
 
+  tmp.dat = tmp.dat[!is.na(tmp.dat$Y),] # ignore missing values
+
   fit_nls = nls.lm(e_par0, fn = fn_obj,
                    lower = lower,
                    upper = upper,
                    tmp.dat = tmp.dat,
                    control = nls.lm.control(maxiter = 1000))
+
+  if (export_ts == TRUE) {
+    # Put time series in data.frame and add it to output:
+    fittedYext = tmp.dat$Y - residuals(fit_nls) #extended cosinor model
+    fittedY = fitted(fit$fit) #cosinor omodel
+    original = tmp.dat$Y #original data
+    time = tmp.dat$time #time
+    time2 = time
+    drops = which(diff(time) < 0) + 1
+    for (k in drops) {
+      time2[k:length(time)] = time2[k:length(time)] + 24
+    }
+    time_across_days = time2
+    cosinor_ts = as.data.frame(cbind(time, time_across_days, original, fittedY, fittedYext)) # data.frame with all signal
+  } else {
+    cosinor_ts = NULL
+  }
+
   ## Estimated exteded cosinor parameters,in the order of
   ## minimum, amplitude, alpha, beta, acrophase
   coef.nls = coef(fit_nls)
@@ -96,7 +118,7 @@ ActExtendCosinor = function(
   DownMesor = acos(e_alpha)/(2*pi/24) + e_acrotime
   MESOR = e_min + e_amp/2
 
-  ret = list("minimum" = e_min,
+  params = list("minimum" = e_min,
              "amp" = e_amp,
              "alpha" = e_alpha,
              "beta" = e_beta,
@@ -106,6 +128,7 @@ ActExtendCosinor = function(
              "DownMesor" = DownMesor,
              "MESOR" = MESOR,
              "ndays" = n.days)
+  ret = list("params" = params, "cosinor_ts" = cosinor_ts)
 
   return(ret)
 
